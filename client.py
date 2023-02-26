e34cb60cbc901c4e674f7df62cfa9cc740f769d1
kcshenoy <59003979+kcshenoy@users.noreply.github.com>
2023-02-26 13:48:28 -0500

    Add files via upload

--- client.py
+++ client.py
@@ -1,6 +1,20 @@
 import sys
 from socket import *
 
+'''
+tcp_neg(addr, n_port, req_code) sends req_code to the server to be checked with 
+its req_code, where:
+
+    addr: IP address/hostname of socket, type str
+    n_port: port where TCP server is hosted, type str
+    req_code: code to be tested for match, type int
+
+req_code is sent to the server, and the response is recorded as r_port. If r_port is 
+negative/invalid, then we exit the script, else we close the connection and return 
+our r_port which we use to create the UDP socket for our transaction phase. If our 
+connection is invalid due to a wrong n_port or addr, we show a connection error and 
+exit the script.
+'''
 def tcp_neg(addr, n_port, req_code):
     try:
         clientSocket = socket(AF_INET, SOCK_STREAM)
@@ -20,6 +34,18 @@ def tcp_neg(addr, n_port, req_code):
         print('CONNECTION ERROR: ' + str(e.args))
         sys.exit(-1)
 
+
+'''
+udp_send_msg(addr, r_port, msg) returns the original message modified by the server,
+where:
+
+    addr: IP address/hostname of socket, type str
+    r_port: port where UDP server is hosted, type int
+    msg: message to be reversed, type str
+
+Create the socket, and then send the message over to the server for it to be reversed.
+We then receive it and return the modified message.
+'''
 def udp_send_msg(addr, r_port, msg):
     print("UDP TRANSACTION")
     serverPort = r_port
@@ -31,6 +57,20 @@ def udp_send_msg(addr, r_port, msg):
     clientSocket.close()
     return modifiedMessage
 
+
+'''
+main() is responsible for running the script, where:
+
+    address: IP address/hostname of socket, type str
+    n_port: port where initial TCP negotiation takes place, type int
+    req_code: request code to be checked with client request code, type int
+    msg: message to be reversed, type str
+
+All 4 parameters are read from input and checked for validity and existence. if both 
+exceptions are not triggered, we create the server socket for the TCP negotiation phase. 
+From the negotiation phase, we use the returned port to create the UDP socket to initiate 
+the transaction phase. 
+'''
 def main():
     try:
         address = str(sys.argv[1])
--- server.py
+++ server.py
@@ -1,11 +1,38 @@
 import sys
 from socket import *
 
+'''
+create_socket(sock_type, n_port) creates a new socket, given parameters sock_type
+and n_port, where:
+
+    sock_type: type of socket, eg. SOCK_DGRAM (UDP), type string
+    n_port: port where server is hosted, type int
+
+Returns the socket binded to n_port of type Socket
+'''
 def create_socket(sock_type, n_port):
     serverSocket = socket(AF_INET,sock_type)
     serverSocket.bind(('',n_port))
     return serverSocket
 
+
+'''
+tcp_neg(serverSocket, req_code, r_port) takes parameters serverSocket, 
+req_code, and r_port, and returns a UDP socket if negotiation with
+client is succesful, where:
+
+    serverSocket: where TCP server socket is listening for client,
+                type Socket
+    req_code: request code which is to be checked by the request code
+            sent by client, type int
+    r_port: port where TCP server is listening for client req_code
+            type int
+
+While running, if the client request code matches our req_code, the server
+returns the port where our UDP server will be waiting for the message to 
+be reversed. If request codes do not match, we send an error code/invalid
+port number to the client and close the connection socket.
+'''
 def tcp_neg(serverSocket, req_code, r_port):
 
     serverSocket.listen(1)
@@ -29,6 +56,12 @@ def tcp_neg(serverSocket, req_code, r_port):
             connectionSocket.send("-1".encode())
             connectionSocket.close()
     
+
+'''
+udp_reverse(sock) decodes the client message, reverses the string, and sends
+it back to the client, where:
+    sock: UDP server socket which communicates with client, type Socket
+'''
 def udp_reverse(sock):
     print('THE UDP SERVER IS READY TO RECEIVE')
     message, clientAddress = sock.recvfrom(1024)
@@ -38,6 +71,17 @@ def udp_reverse(sock):
     sock.sendto(modifiedMessage.encode(), clientAddress)
     
 
+'''
+main() is responsible for running the script, where:
+
+    n_port: port where initial TCP negotiation takes place, type int
+    req_code: request code to be checked with client request code, type int
+
+Both n_port and req_code are read from input and checked for validity and
+existence. if both exceptions are not triggered, we create the welcoming socket
+for the TCP negotiation phase. From the negotiation phase, we use the returned
+UDP socket to initiate the transaction phase. 
+'''
 def main():
     try:
         n_port = int(sys.argv[1])
